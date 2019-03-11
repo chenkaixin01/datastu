06fc2ab9b1960be6a54ecf4ecbed1fa88aa0d847
chenkaixin <chenkaixin@chenkaixin.com>
2019-03-11 21:05:59 +0800

    寻找两个有序数组的中位数

--- leedcode/src/main/java/arrays/MedianOfTwoSortedArrays.java
+++ leedcode/src/main/java/arrays/MedianOfTwoSortedArrays.java
@@ -1,3 +1,5 @@
+package arrays;
+
 /**
  * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
  * <p>
@@ -5,12 +7,20 @@
  * <p>
  * 你可以假设 nums1 和 nums2 不会同时为空。
  */
-class Solution {
+class MedianOfTwoSortedArrays {
+    /**
+     * 1.当两个数组的长度之和取2的余数,余数为1则中位数可以直接返回,如果余数为0,则中位数为最中间两位数的平均数
+     * 2.建立一个数组,数组长度为:(nums1.length + nums2.length) / 2 + 1
+     * 3.按照两个数组的排序存入nums中,当nums满时,取到中位数
+     * @param nums1 有序数组1
+     * @param nums2 有序数组2
+     * @return
+     */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int count1 = 0;
         int count2 = 0;
         int target = (nums1.length + nums2.length) / 2 + 1;
-        boolean flag = true;
+        boolean flag;
         int[] nums = new int[target];
         if (((nums1.length + nums2.length) % 2) == 1) {
             flag = true;
@@ -49,7 +59,7 @@ class Solution {
     public static void main(String[] args) {
         int[] nums1 = {1, 2};
         int[] nums2 = {3, 4};
-        Solution solution = new Solution();
-        System.out.println(solution.findMedianSortedArrays(nums1, nums2));
+        MedianOfTwoSortedArrays medianOfTwoSortedArrays = new MedianOfTwoSortedArrays();
+        System.out.println(medianOfTwoSortedArrays.findMedianSortedArrays(nums1, nums2));
     }
 }
