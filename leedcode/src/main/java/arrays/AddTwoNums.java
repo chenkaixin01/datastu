d610198838bc7196bc97e2b1a3f3b12a2dc8dcda
chenkaixin <chenkaixin@chenkaixin.com>
2019-04-12 13:54:50 +0800

    使用非空链表存储数字，两数相加

--- leedcode/src/main/java/arrays/AddTwoNums.java
+++ leedcode/src/main/java/arrays/AddTwoNums.java
@@ -12,42 +12,44 @@ import java.util.List;
  **/
 public class AddTwoNums {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if(0==l1.val){
-            return l2;
-        }
-        if (0==l2.val){
-            return l1;
-        }
-        List list1 = new ArrayList();
-        List list2 = new ArrayList();
-        add(list1,list2,l1,l2);
-
-        return null;
-    }
-    private void add(List list1,List list2,ListNode l1, ListNode l2){
-        if(l1!=null){
-            list1.add(l1.val);
-        }
-        if(l2!=null){
-            list2.add(l2.val);
-        }
-        if(l1.next==null && l2.next==null){
-            return;
-        }else{
-            if(l1.next==null){
-                 add(list1,list2,l1,l2.next);
-            }else if(l2.next==null){
-                 add(list1,list2,l1.next,l2);
-            }else {
-                add(list1,list2,l1.next,l2.next);
+        int sum = l1.val+l2.val;
+        ListNode result = new ListNode(sum%10);
+        if(null==l1.next && null==l2.next){
+            if(sum>9){
+                result.next=new ListNode(sum/10);
             }
+            return result;
         }
-
-
+        if(null==l1.next){
+            l1.next=new ListNode(0);
+        }
+        if(null==l2.next){
+            l2.next = new ListNode(0);
+        }
+        add(l1.next,l2.next,sum/10,result);
+        return result;
     }
-    private class ListNode {
+
+    private void add(ListNode l1,ListNode l2,int bit,ListNode result){
+        int sum = l1.val+l2.val+bit;
+        result.next = new ListNode(sum%10);
+        if(null != l1.next && null != l2.next){
+            add(l1.next,l2.next,sum/10,result.next);
+        }else if(null != l1.next && null == l2.next){
+            add(l1.next,new ListNode(0),sum/10,result.next);
+        }else if(null == l1.next && null != l2.next){
+            add(new ListNode(0),l2.next,sum/10,result.next);
+        }else{
+            if(sum>9){
+                result.next.next = new ListNode(sum/10);
+            }
+            return;
+        }
+    }
+    public class ListNode {
         int val;
         ListNode next;
         ListNode(int x) { val = x; }
     }
+
 }
